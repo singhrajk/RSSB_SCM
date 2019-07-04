a613e88128925cec66f27fd92ecf2ad9577bd752
Rana Rajput <ranarajput2008@gmail.com>
2019-07-04 18:37:13 +0530

    Restructured the setup and test class

--- src/main/python/browser.py
+++ src/main/python/browser.py
@@ -5,7 +5,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException
 import time
-import os
+
+waitForPresence = 1
+waitForClickable = 2
+waitForPresenceAndClickable = 3
 
 class Browser():
     def __init__(self, command_executor, delay):
@@ -28,75 +31,102 @@ class Browser():
     def __del__(self):
       self._browser.quit()
 
+        # Load a particular page in the browser
     def load_page_in_browser(self, url, errorMsg):
         try:
             self._browser.get(url)
         except:
             print (errorMsg)
+    
+    # Get Element From XPath after when the element is present
+    def get_element_from_xpath(self, xpath, wait):
+        if wait == waitForPresence:
+                WebDriverWait(self._browser, self._delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        elif wait == waitForClickable:
+                WebDriverWait(self._browser, self._delay).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        elif wait == waitForPresenceAndClickable:
+                WebDriverWait(self._browser, self._delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
+                WebDriverWait(self._browser, self._delay).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        time.sleep(1)
+        element = self._browser.find_element_by_xpath(xpath)
+        return element
 
-    def get_element_from_xpath(self, xpath, element_present):
-            WebDriverWait(self._browser, self._delay).until(element_present)
-            time.sleep(1)
-            element = self._browser.find_element_by_xpath(xpath)
-            return element
-
-    def get_element_wait_for_presence(self, xpath):
-            element_present = EC.presence_of_element_located((By.XPATH, xpath))
-            return self.get_element_from_xpath(xpath, element_present)
-
-    def get_element_wait_for_clickable(self, xpath):
-            element_present = EC.element_to_be_clickable((By.XPATH, xpath))
-            return self.get_element_from_xpath(xpath, element_present)
-
+    # Sends Input keys at a particular input on x path after it is present
     def send_inputs_at_xpath(self, xpath, keys, errorMsg):
         try:
-            element = self.get_element_wait_for_presence(xpath)
+            element = self.get_element_from_xpath(xpath, waitForPresence)
             element.send_keys(keys)
         except:
             print (errorMsg)
-
+        
+    # Click the element available at the X Path after it is Present & Clickable
     def click_element_at_xpath(self, xpath, errorMsg):
         try:
-            try:
-                element = self.get_element_wait_for_presence(xpath)
-            except:
-                element = self.get_element_wait_for_clickable(xpath)
-            finally:
-                element.click()
+            element = self.get_element_from_xpath(xpath, waitForPresenceAndClickable)
+            element.click()
         except:
             print (errorMsg)
-
+            
+    # Select group on the screen
     def select_group(self, group_no, tab):
         self.click_element_at_xpath('//*[@id="mnugrp_' + group_no + '"]', "Error while clicking: " + tab + " tab")
-
+    
+    # Select the element in the list, Put the list_no as the position of the tab, put the choice_no as the position of the element
     def select_group_choice(self, list_no, choice_no):
         self.click_element_at_xpath('//*[@id="wdf_root_div_id"]/div[1]/div/div[1]/ul/li[' + list_no + ']/ul/li[' + choice_no + ']/span/span[1]', "Error while clicking Centre Choice")
-
+    
+    def select_group_and_group_choice(self, tab, group_no, list_no, choice_no): 
+        self.select_group(group_no , tab)
+        self.select_group_choice(list_no, choice_no)    
+    
+    # Query the records, this will press the Query Button
     def query_records(self, tab):
         self.click_element_at_xpath('//*[@id="btnQuery"]/i',"Error while quering: " + tab + " tab")
-
+    
+    # Fetch the records, this will press the Get button
     def fetch_records(self, tab):
         self.click_element_at_xpath('//*[@id="btnGet"]',"Error while fetching records for: " + tab + " tab")
-
+    
+    # This will click on the export button on browser
     def export_records_in_excel(self, tab):
         self.click_element_at_xpath('//*[@id="wdf-list-view row-fluid"]/div/div[1]/div/button', "Error while exporting : " + tab + " tab")
-
+    
+    def search_input(self, tab, search_input):
+        self.send_inputs_at_xpath('//*[@id="'+tab+'_Id_lookup"]',search_input, "Error while entering data to search " + tab + " tab")
+    
+    def click_select_button (self, tab):
+        self.click_element_at_xpath('//*[@id="divOptions"]/button',"Error while clicking select button: " + tab + " tab")
+    
+    def click_search_button (self, tab):
+        self.click_element_at_xpath('//*[@id="wdfScreenFieldSet"]/div/div/form/div[1]/fieldset/button',"Error while clicking search button: " + tab + " tab")
+    
+    # This method is to login on page, username , password will come from environment
     def login_on_page(self, url, username, password):
         self.load_page_in_browser(url, "Error Loading the page")
         self.send_inputs_at_xpath('//*[@id="LoginId"]', username, "User Id Error")
         self.send_inputs_at_xpath('//*[@id="Password"]', password, "Password Error" )
-        self.click_element_at_xpath('//*[@id="login-button"]',"Submit Error")
-
-def main():
-    command_executor = os.getenv("CMD_EXECUTOR","")
-    delay = 60
-    browser = Browser(command_executor, delay)
-
-    url = "https://derapps-staging.azurewebsites.net/SCM"
-    username = os.getenv("SITE_LOGIN_USER","")
-    password = os.getenv("SITE_LOGIN_PWD","")
-    browser.login_on_page(url, username, password)
-    time.sleep(10)
-
-if __name__ == "__main__":
-    main()
+        self.click_element_at_xpath('//*[@id="login-button"]',"Login Submit Error")
+    
+    # Method to test the export
+    def test_export(self, tab, group_no, list_no, choice_no):
+        self.select_group_and_group_choice(tab, group_no, list_no, choice_no)
+        self.query_records(tab)
+        self.fetch_records(tab)
+        self.export_records_in_excel(tab)
+    
+    def test_search(self, tab, group_no, list_no, choice_no, search_input):
+        self.select_group_and_group_choice(tab, group_no, list_no, choice_no)
+        self.search_input(tab, search_input)
+        time.sleep(1)
+        self.click_select_button(tab)
+        self.click_search_button(tab)
+        
+    def logout(self):
+        self.click_element_at_xpath('//*[@id="wdf_root_div_id"]/div[1]/div/div[2]/div/div[2]/a[2]',"Logout Error")
+    
+    def test_search_query(self, tab, group_no, list_no, choice_no, search_input):
+        self.select_group_and_group_choice(tab, group_no, list_no, choice_no)
+        self.query_records(tab)
+        self.search_input(tab, search_input)
+        self.click_select_button(tab)
+        self.fetch_records(tab)
--- src/unittest/python/fiddle_tests.py
+++ src/unittest/python/fiddle_tests.py
@@ -4,41 +4,82 @@ import os
 BASE_PATH = os.getenv('SCM_HOME',"")
 sys.path.extend([BASE_PATH + '/src/main/python/'])
 from browser import Browser
-import time
+import time    
 
-def test_export(browser, tab, group_no, list_no, choice_no):
-    browser.select_group(group_no , tab)
-    browser.select_group_choice(list_no,choice_no)
-    browser.query_records(tab)
-    browser.fetch_records(tab)
-    browser.export_records_in_excel(tab)
+deraSCMUrl = "https://derapps-staging.azurewebsites.net/SCM"
+tab_centre= "centre";
+tab_preacher= "preacher";
+lookup_sewadar ="sewadar";
+group_no_centre = "2";
+group_no_preacher = "4";
+list_no_centre = "2"
+list_no_preacher = "3"
 
 class FiddleTest(unittest.TestCase):
-
-    def setUp(self):
+    
+    @classmethod
+    def setUpClass(self):
         """Start web browser"""
-        command_executor = os.getenv("CMD_EXECUTOR","")
         delay = 60
+        command_executor = os.getenv("CMD_EXECUTOR","")
         self._browser = Browser(command_executor, delay)
-
-    def test_login_and_export_center(self):
         try:
-            url = "https://derapps-staging.azurewebsites.net/SCM"
             username = os.getenv("SITE_LOGIN_USER","")
             password = os.getenv("SITE_LOGIN_PWD","")
-            self._browser.login_on_page(url, username, password)
-            test_export(self._browser, "Center" , "2" , "2", "1")
-            time.sleep(2)
+            self._browser.login_on_page(deraSCMUrl, username, password)
         except Exception as ex:
             self.fail(ex)
+        
+    def setUp(self):
+        time.sleep(2)
 
-    def test_login_and_export_preacher(self):
+    # Do Nothing
+    def test_0(self):
+        pass
+    
+    # This will test search the centre schedule
+    def test_1_search_centre_scehdule(self):
         try:
-            url = "https://derapps-staging.azurewebsites.net/SCM"
-            username = os.getenv("SITE_LOGIN_USER","")
-            password = os.getenv("SITE_LOGIN_PWD","")
-            self._browser.login_on_page(url, username, password)
-            test_export(self._browser, "Preacher" , "4" , "3", "1")
-            time.sleep(2)
+            self._browser.test_search(tab_centre, group_no_centre, list_no_centre, "2", "ba")
         except Exception as ex:
             self.fail(ex)
+    
+    def test_2_search_centre_scehdule(self):
+        try:
+            self._browser.test_search_query(tab_centre, group_no_centre, list_no_centre, "1", "bangalore")
+        except Exception as ex:
+            self.fail(ex)
+    
+    # This will test export the centers
+    def test_3_export_center(self):
+        try:
+            self._browser.test_export(tab_centre , group_no_centre , list_no_centre, "1")
+        except Exception as ex:
+            self.fail(ex)
+    
+    def test_4_search_preacher(self):
+        try:
+            self._browser.test_search_query(lookup_sewadar, group_no_preacher, list_no_preacher, "1", "GUL")
+        except Exception as ex:
+            self.fail(ex)
+    
+    # This will test export the preachers
+    def test_5_export_preacher(self):
+        try:
+            self._browser.test_export(tab_preacher , group_no_preacher , list_no_preacher, "1")
+        except Exception as ex:
+            self.fail(ex)
+    
+    @classmethod
+    def tearDownClass(self):
+        """Logout browser"""
+        try:
+            time.sleep(2)
+            self._browser.logout();
+        except Exception as ex:
+            self.fail(ex)
+        time.sleep(4)
+        self._browser = None
+            
+if __name__ == '__main__':
+    unittest.main()
