52e6b38ef181cb13e399138ef0e38c2b11c0a232
raj.singh <raj.singh@grabtaxi.com>
2019-07-05 10:09:27 +0800

    Spell check and formatting

--- src/unittest/python/fiddle_tests.py
+++ src/unittest/python/fiddle_tests.py
@@ -30,13 +30,13 @@ class FiddleTest(unittest.TestCase):
         pass
 
     # This will test search the centre schedule
-    def test_1_search_centre_scehdule(self):
+    def test_1_search_centre_schedule(self):
         try:
             self._browser.test_search(constants.TAB_CENTRE, constants.GROUP_NO_CENTRE, constants.LIST_NO_CENTRE, "2", "ba")
         except Exception as ex:
             self.fail(ex)
 
-    def test_2_search_centre_scehdule(self):
+    def test_2_search_centre_schedule(self):
         try:
             self._browser.test_search_query(constants.TAB_CENTRE, constants.GROUP_NO_CENTRE, constants.LIST_NO_CENTRE, "1", "bangalore")
         except Exception as ex:
@@ -45,7 +45,7 @@ class FiddleTest(unittest.TestCase):
     # This will test export the centers
     def test_3_export_center(self):
         try:
-            self._browser.test_export(constants.TAB_CENTRE , constants.GROUP_NO_CENTRE , constants.LIST_NO_CENTRE, "1")
+            self._browser.test_export(constants.TAB_CENTRE, constants.GROUP_NO_CENTRE, constants.LIST_NO_CENTRE, "1")
         except Exception as ex:
             self.fail(ex)
 
@@ -58,7 +58,7 @@ class FiddleTest(unittest.TestCase):
     # This will test export the preachers
     def test_5_export_preacher(self):
         try:
-            self._browser.test_export(constants.TAB_PREACHER , constants.GROUP_NO_PREACHER , constants.LIST_NO_PREACHER, "1")
+            self._browser.test_export(constants.TAB_PREACHER, constants.GROUP_NO_PREACHER, constants.LIST_NO_PREACHER, "1")
         except Exception as ex:
             self.fail(ex)
 
