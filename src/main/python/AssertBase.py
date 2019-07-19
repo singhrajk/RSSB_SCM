9bd01ddf884a0feeb420df3d3ba4a831b1191430
Rana Rajput <ranarajput2008@gmail.com>
2019-07-19 08:49:14 +0530

    Restructures Successful, Working on Asserts

--- src/main/python/AssertBase.py
+++ src/main/python/AssertBase.py
@@ -11,7 +11,7 @@ from BrowserBase import BrowserBase
 class Assert(BrowserBase):
     
     def test_if_element_selected(self, row_no, col_no):
-        xpath = (XPATH.PREACHER_LANGUAGE.replace(constants.DUMMY_ROW_NO, row_no).replace(constants.DUMMY_COLUMN_NO, col_no))     
+        xpath = (XPATH.SCREENFIELD_TABLE_INPUT.replace(constants.DUMMY_ROW_NO, row_no).replace(constants.DUMMY_COLUMN_NO, col_no))     
         element = self.get_element_from_xpath(xpath, constants.WAIT_FOR_PRESENCE)
         if not element.is_selected():
             assert False, "\tElement was not selected at row: " + row_no + " and column: " + col_no
@@ -28,7 +28,7 @@ class Assert(BrowserBase):
     
     def test_element_value_screenfield(self, row_no, col_no, expected_value, by="value"):
         xpath = (XPATH.SCREENFIELD_TABLE_INPUT.replace(constants.DUMMY_ROW_NO, row_no).replace(constants.DUMMY_COLUMN_NO, col_no))
-        print ("Checking expected and actual values at row: " + row_no + " and column: " + col_no)
+        print ("\tChecking expected and actual values at row: " + row_no + " and column: " + col_no)
         self.test_element_value_xpath(xpath, expected_value, by)
 
     def test_element_value_lookup(self, lookup_id, expected_value, by="value"):
